166d297881d10fe1605ac3e561dfee489a8c8d2a
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2022-04-17 06:19:32 +0700

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -141,8 +141,8 @@ sideBarInfo.addGroup('note', 'Don\'t make nsfw and furry arts.')
 const footer = new StyledElement('footer shadow')
 footer.addHtml = `<div class="center">
   <div>Contacts</div>
-  <div>Discord:&nbsp<span class="link">L' kk#6790</span></div>
-  <div>Github:&nbsp<a class="link" target="_blank" href="https://github.com/Lkk9">Lkk9</a>&nbsp|&nbsp<a class="link" target="_blank" href="https://github.com/Lkk9/pngToMsav">Source Code</a></div>
+  <div>Discord:&nbspOriginal Made by <span class="link">L' kk#6790</span></div>
+  <div>Github:&nbsp<a class="link" target="_blank" href="https://github.com/WilloIzCitron">WilloIzCitron</a>&nbsp|&nbspOriginal Made By:<a class="link" target="_blank" href="https://github.com/Lkk9">Lkk9</a>&nbsp|&nbsp<a class="link" target="_blank" href="https://github.com/WilloIzCitron/pngToMsav">Source Code</a></div>
   <div class="cop" style="user-select: none;">Copyright (c) 2022 Copyright Holder All Rights Reserved.</div>
 </div>`
 
